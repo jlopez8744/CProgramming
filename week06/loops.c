0553e14f941aa6ddda4177d3627265a5ec299271
jlopez8744 <jlopez8744@floridapoly.edu>
2025-02-13 17:15:07 +0000

    week06_loops_finish

--- week06/loops.c
+++ week06/loops.c
@@ -1,27 +1,45 @@
 #include <stdio.h>
-#include <unistd.h>
+#include <unistd.h> //usleep function
+#include <stdlib.h> //rand function
+#include <time.h> //time function
 
 //XL7XJ
 
 void printHello(int);
+int farenheitToCelcius(int);
+void printTemperatures(int, int);
+void printNRandom(int);
 
 void main() {
-    printHello(100);
-}
-
-int largest(int n1, int n2, int n3) {
-    if (n1 >= n2 && n1 >= n3) {
-        return n1;
-    } else if (n2 >= n1 && n2 >= n3) {
-        return n2;
-    } else {
-        return n3;
-    }
+    srand(time(0));
+    printNRandom(5);
 }
 
 void printHello(int n) {
     for (int i = 0; i < n; i++) {
         printf("Hello, World! : %d\n", i+1);
+        usleep(30000);
+    }
+}
+
+int farenheitToCelcius(int f) {
+    return (f - 32) * 5 / 9;
+}
+
+void printTemperatures(int min, int max) {
+    if (min > max) {
+        puts("Invalid minimum and maximum notation");
+    }
+    for (int f = min; f <= max; f++) {
+        int c = farenheitToCelcius(f);
+        printf("%d F -> %d C\n", f, c);
+        usleep(30000);
+    }
+}
+
+void printNRandom(int n) {
+    for (int i = 0; i < n; i++) {
+        printf("%d\n", rand() % 10);
         usleep(30000);
     }
 }
